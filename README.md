c9f344c337fffb16ac3db1019ae766f23a2a80f9
Jesse Reynolds <jesse@va.com.au>
2016-04-08 11:32:33 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# puppet_certificate_extensions_facts
-Provides custom fact to include all puppet certificate extensions as facts
+# Puppet Certificate Extensions Facts
+
+This puppet module provides a custom fact to include all puppet certificate extensions as facts.
+
+Facts added correspond to each of the X509 certificate extensions
+under Puppet's ppRegCertExt oid namespace, `1.3.6.1.4.1.34380.1.1`. When there are short names defined in
+the puppet agent code in `Puppet::SSL::Oids::PUPPET_OIDS` the facts will appear with
+the short names as the fact names. Eg:
+
+```
+$ facter pp_instance_id
+i-8da75c11
+```
+
+They will also always appear as facts named with the oid prefixed with `certificate_extension_`, and with periods translated to underscores Eg:
+
+```
+$ facter certificate_extension_1_3_6_1_4_1_34380_1_1_2
+i-8da75c11
+```
+
+See https://docs.puppet.com/puppet/latest/reference/ssl_attributes_extensions.html#puppet-specific-registered-ids for the list of oids with short names defined in the puppet agent.
+
+Author: Jesse Reynolds; Puppet, Inc.
+
+This fact is subject to the license of this module as per LICENSE.md
+
---
+++ lib/facter/certificate_extensions.rb
@@ -0,0 +1,54 @@
+# This adds custom facts that correspond to each of the X509 certificate extensions
+# under Puppet's ppRegCertExt oid namespace, "1.3.6.1.4.1.34380.1.1". When there are short names defined in
+# the puppet agent code in Puppet::SSL::Oids::PUPPET_OIDS the facts will appear with
+# the short names as the fact names. Eg:
+#
+#   $ facter pp_instance_id
+#   i-8da75c11
+#
+# They will also always appear as facts named with oid prefixed with 'certificate_extension_', and with periods translated to underscores Eg:
+#
+#   $ facter certificate_extension_1_3_6_1_4_1_34380_1_1_2
+#   i-8da75c11
+#
+# See https://docs.puppet.com/puppet/latest/reference/ssl_attributes_extensions.html#puppet-specific-registered-ids for the list of oids with short names defined in the puppet agent.
+#
+# Author: Jesse Reynolds; Puppet, Inc.
+#
+# This fact is subject to the license of this module as per LICENSE.md
+
+require 'openssl'
+require 'puppet'
+require 'puppet/ssl/oids'
+
+Puppet.initialize_settings
+
+certname = Puppet.settings[:certname]
+ssldir   = Puppet.settings[:ssldir]
+
+oids = {}
+Puppet::SSL::Oids::PUPPET_OIDS.each {|o|
+  oids[o[0]] = o[1]
+}
+
+raw = File.read("#{ssldir}/certs/#{certname}.pem")
+
+cert = OpenSSL::X509::Certificate.new(raw)
+
+cert.extensions.each {|e|
+  next unless e.oid =~ /^1\.3\.6\.1\.4\.1\.34380\.1\.1/
+  oid_name = "certificate_extension_#{e.oid.gsub(/\./, '_')}"
+  Facter.add(oid_name) do
+    setcode do
+      e.value[2..-1]
+    end
+  end
+  short_name = oids[e.oid]
+  next unless short_name
+  Facter.add(short_name) do
+    setcode do
+      e.value[2..-1]
+    end
+  end
+}
+
